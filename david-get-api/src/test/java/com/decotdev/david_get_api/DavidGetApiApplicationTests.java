07326d7e4e0f096cc3a561da192c1b82a5085a1b
DecotDev <decot013@gmail.com>
2025-03-26 20:46:48 +0100

    Update

---
+++ david-get-api/src/test/java/com/decotdev/david_get_api/DavidGetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decotdev.david_get_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavidGetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
