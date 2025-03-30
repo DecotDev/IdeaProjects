4d121305aae5714fbbd9671e09f0bcd184eeaf5e
DecotDev <decot013@gmail.com>
2025-03-30 21:08:59 +0200

    upload

--- david-get-api/src/main/java/com/decotdev/david_get_api/Question.java
+++ david-get-api/src/main/java/com/decotdev/david_get_api/Question.java
@@ -1,49 +1,32 @@
 package com.decotdev.david_get_api;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.beans.factory.annotation.Value;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Arrays;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Question {
-    public String difficulty;
-    public String category;
-    @Value("question_text")
-    public String question;
-    public String correct_answer;
-    public String[] incorrect_answers;
 
+    private String difficulty;
+    private String category;
+
+    @JsonProperty("question")  // Ensure JSON field is mapped correctly
+    private String questionText;
+
+    @JsonProperty("correct_answer")
+    private String correctAnswer;
+
+    @JsonProperty("incorrect_answers")
+    private String[] incorrectAnswers;
+
+    // Getters and Setters
     public String getDifficulty() {
         return difficulty;
     }
 
     public void setDifficulty(String difficulty) {
         this.difficulty = difficulty;
-    }
-
-    public String[] getIncorrect_answers() {
-        return incorrect_answers;
-    }
-
-    public void setIncorrect_answers(String[] incorrect_answers) {
-        this.incorrect_answers = incorrect_answers;
-    }
-
-    public String getCorrect_answer() {
-        return correct_answer;
-    }
-
-    public void setCorrect_answer(String correct_answer) {
-        this.correct_answer = correct_answer;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
     }
 
     public String getCategory() {
@@ -54,15 +37,38 @@ public class Question {
         this.category = category;
     }
 
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String[] getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public void setIncorrectAnswers(String[] incorrectAnswers) {
+        this.incorrectAnswers = incorrectAnswers;
+    }
+
     @Override
     public String toString() {
         return "Question{" +
                 "difficulty='" + difficulty + '\'' +
                 ", category='" + category + '\'' +
-                ", question='" + question + '\'' +
-                ", correct_answer='" + correct_answer + '\'' +
-                ", incorrect_answers=" + Arrays.toString(incorrect_answers) +
+                ", questionText='" + questionText + '\'' +
+                ", correctAnswer='" + correctAnswer + '\'' +
+                ", incorrectAnswers=" + Arrays.toString(incorrectAnswers) +
                 '}';
     }
-
 }
